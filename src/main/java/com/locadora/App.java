84a7e29cc072c0c5f2e4f9f6cca720673f906744
MarcosEsmeraldino <marcosesmeraldino@gmail.com>
2019-04-24 13:25:00 -0300

    end services (24/04/2019)

--- src/main/java/com/locadora/App.java
+++ src/main/java/com/locadora/App.java
@@ -8,12 +8,4 @@ public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
-    
-    // O sistema deve permitir:
-    // criação de usuários
-    // logon e logoff de um usuário
-    // listagem de filmes disponíveis
-    // locação de um filme
-    // devolução de um filme
-    // pesquisa de filme pelo título
 }
